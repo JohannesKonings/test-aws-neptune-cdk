5a17f60ff535215b6f0035fbbf4870843cf488b4
Johannes Konings <johannes.konings@outlook.com>
2021-01-27 18:30:32 +0100

    neptune db cluster added

--- neptune/lib/neptune-stack.ts
+++ neptune/lib/neptune-stack.ts
@@ -1,9 +1,18 @@
 import * as cdk from '@aws-cdk/core';
+const {
+  CfnDBCluster,
+  CfnDBSubnetGroup,
+  CfnDBInstance,
+  CfnDBParameterGroup,
+  CfnDBClusterParameterGroup
+} = require("@aws-cdk/aws-neptune");
 
 export class NeptuneStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const dbCluster = new CfnDBCluster(this, "NeptuneDBCluster", {
+
+    });
   }
 }
