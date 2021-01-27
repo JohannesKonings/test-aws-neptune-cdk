d0a3a5cbf1701df14126cc2ccd3e7ab0699f1998
Johannes Konings <johannes.konings@outlook.com>
2021-01-27 18:01:59 +0100

    changed to typescript

--- neptune/bin/neptune.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-const cdk = require('@aws-cdk/core');
-const { NeptuneStack } = require('../lib/neptune-stack');
-
-const app = new cdk.App();
-new NeptuneStack(app, 'NeptuneStack');
