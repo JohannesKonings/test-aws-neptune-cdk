088a66277ccc3a1075110f37c06eb9d281d3f47c
Johannes Konings <johannes.konings@outlook.com>
2021-04-05 19:05:51 +0200

    add neptune stack

--- neptune/bin/neptune.ts
+++ neptune/bin/neptune.ts
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { NeptuneStack } from '../lib/neptune-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { NeptuneStack } from "../lib/neptune-stack";
 
 const app = new cdk.App();
-new NeptuneStack(app, 'NeptuneStack');
+new NeptuneStack(app, "NeptuneStack", {
+  env: {
+    region: "us-east-1",
+  },
+});
---
+++ neptune/lambda-fns/createPost.ts
@@ -0,0 +1,21 @@
+const gremlin = require("gremlin");
+import Post from "./Post";
+
+const DriverRemoteConnection = gremlin.driver.DriverRemoteConnection;
+const Graph = gremlin.structure.Graph;
+const uri = process.env.WRITER;
+
+async function createPost(post: Post) {
+  let dc = new DriverRemoteConnection(`wss://${uri}/gremlin`, {});
+  const graph = new Graph();
+  const g = graph.traversal().withRemote(dc);
+
+  await g
+    .addV("posts")
+    .property("title", post.title)
+    .property("content", post.content)
+    .next();
+  dc.close();
+  return post;
+}
+export default createPost;
